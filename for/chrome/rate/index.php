ce5efde2da79529ed4a22df82cf87b1fc58c1bd7
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2023-02-24 21:43:16 +0100

    feat: :children_crossing: only on rating page if user is not on mobile

--- for/chrome/rate/index.php
+++ for/chrome/rate/index.php
@@ -1,2 +1,10 @@
 <?php
-header('Location: https://chrome.google.com/webstore/detail/chatgpt-optimizer-superch/fmnbpdmndaabkihejhgdpdpjihokbhlb/reviews/');
+function isMobileDevice() {
+          return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER['HTTP_USER_AGENT']);
+}
+
+if(isMobileDevice()){
+          header('Location: ../../../../');
+} else {
+          header('Location: https://chrome.google.com/webstore/detail/chatgpt-optimizer-superch/fmnbpdmndaabkihejhgdpdpjihokbhlb/reviews/');
+}
