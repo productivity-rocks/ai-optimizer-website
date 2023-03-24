0bb8740647e68d5b27736d364ee254810e3d3390
jorit.vasconezgerlach <jorit@vasconezgerlach.de>
2023-03-24 13:13:47 +0100

    feat: :zap: improved uninstall page to get more user feedback
    updated gid, more catchy titles, move mail field above text field, ask user twice if he really don't want to fill a mail, block double content

--- assets/js/general.js
+++ assets/js/general.js
@@ -47,3 +47,49 @@ function toast(msg, data = {}) {
         }, 2000);
     }, 100);
 }
+
+// >> open popup
+function popup(html, style, callback, name) {
+    const popup = document.createElement("section");
+    popup.id = "popup";
+    popup.style.cssText =
+        "top: 0; left: 0; position: fixed; height: 100vh; width: 100vw; z-index: 10000 !important; display: grid; place-items:center;";
+
+    const popupBackground = document.createElement("div");
+    popupBackground.style.cssText =
+        "position: absolute; width: 100%; height: 100%; background: hsl(0deg 0% 12% / 50%); z-index: -1; cursor: pointer;";
+    popupBackground.onclick = () => popup.remove();
+    popup.appendChild(popupBackground);
+
+    let noNameClass = (name) ? name : 'noName';
+    const popupInnerContainer = document.createElement("div");
+    popupInnerContainer.innerHTML = `
+        <div class="gpt-optimizer gpto-popupHeader ${noNameClass}">
+            <span>${name}</span>
+            <button><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
+        </div>
+        <div class="gpt-optimizer gpto-popupContentWrap">
+            ${html}
+        </div>`;
+    popupInnerContainer.id = "toastHTML";
+    popupInnerContainer.style.cssText =
+        "max-height: 400px; max-width: 600px; width: 100%; min-height: 200px; height: fit-content; padding-bottom: 1rem; overflow-x: scroll; display: flex; flex-direction: column;";
+    popupInnerContainer.className = "gpt-optimizer gpto-popupInner";
+    popup.appendChild(popupInnerContainer);
+
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = style;
+    popup.appendChild(styleElement);
+
+    document.body.appendChild(popup);
+
+    popup.querySelector(".gpto-popupInner > div:nth-child(1) > button").addEventListener("click", () => {
+        popup.remove();
+    });
+
+    popup.close = ()=>{
+        popup.remove();
+    }
+
+    callback(popup);
+}
--- uninstallation/page.js
+++ uninstallation/page.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", () => {
+    let mailSend = false;
+
     const button = document.querySelector("button");
     const textarea = document.querySelector("textarea");
     const mail = document.querySelector("input[type='email']");
@@ -26,55 +28,111 @@ window.addEventListener("load", () => {
         let pMail = mail.value;
 
         if (!pMessage) {
-            toast("please fill fields");
+            toast("Please Fill Fields");
             return;
         }
 
         if(pMail === undefined || pMail === '') {
             pMail = 'no-mail-provided@productivity.rocks';
+            popup(`
+                <h4>Really don't wan't to be contacted?</h4>
+                <email></email>
+                <div class="buttons">
+                    <button class="outline noMail">No Mail</button>
+                    <button class="prominent submit">Submit</button>
+                </div>
+            `, `
+                #popup .gpto-popupHeader {
+                    display: none;
+                }
+                #popup .gpto-popupContentWrap {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1.2rem;
+                }
+                #popup input {
+                    width: 100%;
+                }
+                #popup .buttons {
+                    display: flex;
+                    gap: .4rem;
+                }
+                #popup .buttons button {
+                    height: 38px;
+                    padding: 2px 10px;
+                }
+            `, popup=>{
+                popup.querySelector('email').parentNode.replaceChild(document.querySelector('#email'), popup.querySelector('email'));
+
+                popup.querySelector('.noMail').addEventListener('click', ()=>{
+                    if(document.querySelector('#email').value && document.querySelector('#email').value.length > 0) {
+                        pMail = document.querySelector('#email').value;
+                    }
+                    sendFeedback();
+                    document.querySelector('#feedbackButton').style.display = 'none';
+                    popup.remove()
+                })
+                popup.querySelector('.submit').addEventListener('click', ()=>{
+                    if(document.querySelector('#email').value && document.querySelector('#email').value.length > 0) {
+                        pMail = document.querySelector('#email').value;
+                    }
+                    sendFeedback();
+                    document.querySelector('#feedbackButton').style.display = 'none';
+                    popup.remove()
+                })
+            }, "Just Confirm")
+        } else {
+            sendFeedback();
         }
 
-        // Set Data as JSON
-        const data = {
-            mail: pMail,
-            type: "feedback",
-            message: pMessage,
-        };
-
-        // Create XHR Request
-        var xhr = new XMLHttpRequest();
-
-        // Setup XHR Request
-        var xhrURL = "https://productivity.rocks/tool/ai-optimizer/api/contact/";
-        xhr.open("POST", xhrURL, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Accept", "application/json");
-
-        // On XHR Request Change
-        xhr.onreadystatechange = function () {
-            // On XHR Ready
-            if (xhr.readyState === 4) {
-                try {
-                    // response
-                    var response = JSON.parse(xhr.responseText);
-                    if (response["error"]) {
-                        // not send
-                        toast("not send", { type: "Error: Try again!" });
-                    } else if (!response["error"]) {
-                        // send
-                        toast("Sent: We'll answer asap!");
-                        textarea.value = "";
-                        featureTable.scrollIntoView({ behavior: "smooth" });
-                    } else {
-                        throw new Error(`No information from server!`);
-                    }
-                } catch (error) {
-                    console.log(error);
-                    toast("Server Error: We' fix it!", { type: "error" });
-                }
+        function sendFeedback() {
+            if(mailSend) {
+                toast('Mail already sent!')
+                return;
             }
-        };
-        // Send XHR Request
-        xhr.send(JSON.stringify(data));
+            // Set Data as JSON
+            const data = {
+                mail: pMail,
+                type: "feedback",
+                message: pMessage,
+            };
+
+            // Create XHR Request
+            var xhr = new XMLHttpRequest();
+
+            // Setup XHR Request
+            var xhrURL = "https://productivity.rocks/tool/ai-optimizer/api/contact/";
+            xhr.open("POST", xhrURL, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader("Accept", "application/json");
+
+            // On XHR Request Change
+            xhr.onreadystatechange = function () {
+                // On XHR Ready
+                if (xhr.readyState === 4) {
+                    try {
+                        // response
+                        var response = JSON.parse(xhr.responseText);
+                        if (response["error"]) {
+                            // not send
+                            toast("Not Send", { type: "Error: Try again!" });
+                        } else if (!response["error"]) {
+                            // send
+                            toast("Sent: We'll answer asap!");
+                            textarea.value = "";
+                            mailSend = true;
+                            featureTable.scrollIntoView({ behavior: "smooth" });
+                        } else {
+                            throw new Error(`No information from server!`);
+                        }
+                    } catch (error) {
+                        console.log(error);
+                        toast("Server Error: We' fix it!", { type: "error" });
+                    }
+                }
+            };
+            // Send XHR Request
+            xhr.send(JSON.stringify(data));
+        }
     };
 });
